bcae51ac7c550c11907927774c12c7c1aa32c283
ZAIN-mashhood <zainmashhood9966@gmail.com>
2024-06-18 15:03:52 +0500

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./App.css";
+import TicTacToe from './TicTacYoe/TicTacToe';
+
+const App = () => {
+
+  return (
+    <>
+      <TicTacToe /> 
+    </>
+  )
+}
+
+export default App;
